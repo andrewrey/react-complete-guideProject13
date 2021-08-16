3543a48e630448b1aa1d9c0c46e352b2fb78af29
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-16 15:08:47 -0700

    created dummy product data array and dynamically output them with ProductItem component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,46 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "The best book",
+    description: "Best book you can read today",
+  },
+  {
+    id: "p2",
+    price: 10,
+    title: "Tree",
+    description: "A tree for your room",
+  },
+  {
+    id: "p3",
+    price: 12,
+    title: "Stuffed Teddy Bear",
+    description: "The only teddy you will ever need",
+  },
+  {
+    id: "p4",
+    price: 11,
+    title: "Slippers",
+    description: "Keep your feet warm with the warmest slippers",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            title={item.title}
+            price={item.price}
+            description={item.desciprtion}
+            key={item.id}
+          />
+        ))}
       </ul>
     </section>
   );
