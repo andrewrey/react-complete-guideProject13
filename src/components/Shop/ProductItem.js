1176717548054d28ecf89c39593f034a0a28ba8c
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-16 15:32:35 -0700

    added onClick event to button on productItem

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,9 +1,14 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
-
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
+  const addItemHandler = () => {
+    dispatch(cartActions.addItemToCart({ title, price, id }));
+  };
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +18,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addItemHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
